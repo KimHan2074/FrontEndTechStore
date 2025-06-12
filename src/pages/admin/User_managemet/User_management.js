4dc7586deb265ee05284acb4628352b06cad6c2d
HuuHau235 <hhai98940@gmail.com>
2025-06-12 09:22:59 +0700

    feature/62 - structure file

---
+++ src/pages/admin/User_managemet/User_management.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class User_management extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default User_management;
