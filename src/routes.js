f9f43ed19236f8f0ca99e5859e677d582154a64d
KimHan2074 <ngothikimhan.2004@gmail.com>
2025-06-13 17:49:05 +0700

    create file routes, header, userLayout, update file app.js

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,34 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import App from "./App";
+import { Route, Routes } from "react-router-dom";
+// import UserLayout from "./layouts/User/UserLayout.js";
+// import AuthLayout from "./layouts/AuthLayout.jsx";
+// import AdminLayout from "./layouts/AdminLayout.jsx";
 
-const Routes = () => (
-    <Switch>
-        <Route path="/" component={App} />
-    </Switch>
-);
+function AppRoutes() {
+  return (
+    <Routes>
+      {/* student layout */}
+      {/* <Route element={<UserLayout />}>
+        <Route path="/student/header" element={<Header />} /> */}
+        {/* Minh họa */}
+      {/* </Route> */}
 
-export default Routes;
+      {/* User layout */}
+      {/* <Route element={<AuthLayout />}>
+        <Route path="/" element={<Login />} /> */}
+        {/* register, reset password */}
+      {/* </Route> */}
+
+      {/* admin layout */}
+      {/* <Route element={<AdminLayout/>}>
+        <Route path="/admin/dashboard" element={<Dashboard/>}/> */}
+       {/* user management, .... */}
+      {/* </Route> */}
+
+        {/* <Route path="/student/header" element={<Header />} /> */}
+      
+    </Routes>
+  );
+}
+
+export default AppRoutes;
