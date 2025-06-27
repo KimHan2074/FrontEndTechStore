7676f0c674c48da78abf4e7196f0bcb164b53fc4
HuuHau235 <hhai98940@gmail.com>
2025-06-27 14:19:14 +0700

    feature/36 - update

--- src/pages/user/HomePage/Homepage.js
+++ src/pages/user/HomePage/Homepage.js
@@ -3,7 +3,6 @@ import PromoBanner from '../../../components/user/Homepage/PromoBanner'
 import CategoryItemHome from '../../../components/user/Homepage/CategoryItemHome'
 import FeaturedProductSection from '../../../components/user/Homepage/FeaturedProductSection'
 import BestDealSection from '../../../components/user/Homepage/BestDealSection'
-import './HomePage.css'
 export default class HomePage extends Component {
   render() {
     return (
