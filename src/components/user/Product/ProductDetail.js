bd59f920e183688c3ed9b4e495d77a4e83acf72f
KimHan2074 <ngothikimhan.2004@gmail.com>
2025-07-24 11:00:28 +0700

    update

--- src/components/user/Product/ProductDetail.js
+++ src/components/user/Product/ProductDetail.js
@@ -127,7 +127,7 @@ const ProductDetail = () => {
   };
 
   const handleProductClick = (productId) => {
-    navigate(`${apiUrl}/user/product-detail/${productId}`);
+    navigate(`/user/product-detail/${productId}`);
   };
 
   useEffect(() => {
