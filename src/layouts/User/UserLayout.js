216c55fa5778e019a9f93802879d1a4a3c28c8a4
KimHan2074 <ngothikimhan.2004@gmail.com>
2025-08-19 13:34:07 +0700

    add userLayout.css

--- src/layouts/User/UserLayout.js
+++ src/layouts/User/UserLayout.js
@@ -1,16 +1,34 @@
+// import { Outlet } from 'react-router-dom';
+// import Header from '../../components/user/Header/Header';
+// import Footer from '../../components/user/Footer/Footer';
+
+// function UserLayout() {
+
+//     return (
+//         <>
+//             <Header />
+//             <Outlet />
+//             <Footer />
+//         </>
+
+//     );
+// }
+// export default UserLayout;
+
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/user/Header/Header';
 import Footer from '../../components/user/Footer/Footer';
+import './UserLayout.css';
 
 function UserLayout() {
-
     return (
-        <>
+        <div className="layout">
             <Header />
-            <Outlet />
+            <main className="content">
+                <Outlet />
+            </main>
             <Footer />
-        </>
-
+        </div>
     );
 }
 export default UserLayout;
